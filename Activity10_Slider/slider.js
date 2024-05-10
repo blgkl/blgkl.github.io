21a17d2a538fc2788895264b70004db2d90e2237
blgkl <147739300+blgkl@users.noreply.github.com>
2024-05-10 23:10:40 +0300

    Add files via upload

---
+++ Activity10_Slider/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider( {
+			  randomStart:true,
+			  pause:3000,
+			  pager:true,
+			  pagerType:'short',
+			  auto: true,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  slideWidth: 250,
+			  slideMargin: 10
+			});
+    	});
